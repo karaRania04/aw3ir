7221d06b6e18c19a7aa3fb5ecdb17eba9eaf9147
karaRania04 <92273705+karaRania04@users.noreply.github.com>
2022-01-24 11:57:46 +0100

    oui
    oui

--- tp6/meteo-angular/src/app/app.component.ts
+++ tp6/meteo-angular/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'meteo-angular';
+  title = '⛅ Weather webapp @Master 3ir²';
 }
