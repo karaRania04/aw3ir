8ce0ae43ac3c2e04d7d2324ffcca19c404534d5e
karaRania04 <92273705+karaRania04@users.noreply.github.com>
2022-01-31 10:07:12 +0100

    oui
    oui

--- tp6/meteo-angular/src/app/services/meteo.service.ts
+++ tp6/meteo-angular/src/app/services/meteo.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { MeteoItem } from '../meteoItem';
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
 
 @Injectable({ providedIn: 'root' })
 export class MeteoService {
